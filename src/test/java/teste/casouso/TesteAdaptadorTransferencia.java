979fb9068cfbc8879b84bf35724d528a2da521e7
Everton Fabiano Koga <ekoga@sisprimedobrasil.com.br>
2023-10-11 10:04:41 -0300

    teste pesquisa de conta com numero nulo

--- src/test/java/teste/casouso/TesteAdaptadorTransferencia.java
+++ src/test/java/teste/casouso/TesteAdaptadorTransferencia.java
@@ -1,19 +1,30 @@
 package teste.casouso;
 
+import br.com.conta.sistema.casouso.porta.PortaTransferencia;
+import br.com.conta.sistema.dominio.modelo.NegocioException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import javax.inject.Inject;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Caso de Uso - Serviço de Transferência")
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = Build1.class)
 public class TesteAdaptadorTransferencia {
+    @Inject
+    PortaTransferencia porta;
 
     @Test
-    @DisplayName("teste")
+    @DisplayName("pesquisa conta com número nulo")
     void teste1() {
-        System.out.println("oi");
+        try {
+            var conta = porta.getConta(null);
+            assertTrue(conta == null, "Conta deve ser nula");
+        } catch (NegocioException e) {
+            fail("Deva carregar uma conta nula.");
+        }
     }
 }
