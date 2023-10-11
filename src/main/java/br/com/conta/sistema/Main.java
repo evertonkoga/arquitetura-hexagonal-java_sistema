2a7620212d2ed6199b84a283c8ee6b4e4168c645
Everton Fabiano Koga <ekoga@sisprimedobrasil.com.br>
2023-10-11 08:53:55 -0300

    commit inicial

---
+++ src/main/java/br/com/conta/sistema/Main.java
@@ -0,0 +1,7 @@
+package br.com.conta.sistema;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/teste/unidade/dominio/modelo/TesteCreditoConta.java
@@ -0,0 +1,35 @@
+package teste.unidade.dominio.modelo;
+
+import br.com.conta.sistema.dominio.modelo.Conta;
+import br.com.conta.sistema.dominio.modelo.NegocioException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+@DisplayName("Regra de Crédito de Conta")
+public class TesteCreditoConta {
+
+    BigDecimal cem = new BigDecimal(100);
+    Conta contaValida;
+
+    @BeforeEach
+    void prepara() {
+        contaValida = new Conta(1, cem, "Rebeca");
+    }
+    // negativos
+    @Test
+    @DisplayName("valor crédito nulo como obrigatório")
+    void teste1() {
+        try {
+            contaValida.creditar(null);
+            fail("valor crédito obrigatório");
+        } catch (NegocioException e) {
+            assertEquals(e.getMessage(), "Valor crédito é obrigatório.");
+            System.out.println(e.getMessage());
+        }
+    }
+}
