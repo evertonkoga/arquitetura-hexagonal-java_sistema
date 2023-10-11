ce57191b7c106b9609188f6abc66bda6dd185a4b
Everton Fabiano Koga <ekoga@sisprimedobrasil.com.br>
2023-10-11 09:44:55 -0300

    Implementado porta de caso de uso para controlador

---
+++ src/main/java/br/com/conta/sistema/casouso/porta/PortaTransferencia.java
@@ -0,0 +1,10 @@
+package br.com.conta.sistema.casouso.porta;
+
+import br.com.conta.sistema.dominio.modelo.Conta;
+import java.math.BigDecimal;
+
+// Responsável por definir a porta de entrada (driver) de operações para caso de uso de transferência.
+public interface PortaTransferencia {
+    Conta getConta(Integer numero);
+    void transferir(Integer contaDebito, Integer contaCredito, BigDecimal valor);
+}
